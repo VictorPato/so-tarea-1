5ecf0c482bdd682a1981d92b061f7f71493d39d2
VictorPato <victor.garrido.u@gmail.com>
2018-10-16 18:44:07 -0300

    avances

--- transbordo.c
+++ transbordo.c
@@ -1,14 +1,118 @@
 #include <nSystem.h>
+#include "transbordo.h"
 
-void inicializar(int p){
+nMonitor m; 
+int* enPargua;
+int barcosEnPargua;
+int proxConsumidoPargua;
+int proxProducidoPargua;
 
+int* enChacao;
+int barcosEnChacao;
+int proxConsumidoChacao;
+int proxProducidoChacao;
+int p;
+
+
+
+void inicializar(int P){
+	m = nMakeMonitor();
+	p = P;
+	// Inicializar las cosas de pargua
+	enPargua = nMalloc(sizeof(int) * p);
+	for(int i = 0; i < p; i++){
+		enPargua[i] = i;
+	}
+	barcosEnPargua = p;
+	proxConsumidoPargua = 0;
+	proxProducidoPargua = 0;
+	// Inicializar las cosas de chacao
+	enChacao = nMalloc(sizeof(int) * p);
+	barcosEnChacao = 0;
+	proxProducidoChacao = 0;
+	proxProducidoChacao = 0;
 }
+
 void finalizar(){
-
+	nDestroyMonitor(m);
+	nFree(enPargua);
+	nFree(enChacao);
 }
+
+int consumirEnChacao(){
+	//nPrintf("Consumiendo en chacao\n");
+	int barco;
+	nEnter(m);
+	while(barcosEnChacao==0){
+		nWait(m);
+	}
+	barco = enChacao[proxConsumidoChacao];
+	proxConsumidoChacao = (proxConsumidoChacao + 1) % p;
+	barcosEnChacao--;
+	nNotifyAll(m);
+	nExit(m);
+	return barco;
+}
+
+int consumirEnPargua(){
+	//nPrintf("Consumiendo en pargua\n");
+	int barco;
+	nEnter(m);
+	//nPrintf("Barcos en pargua: %d\n",barcosEnPargua);
+	while(barcosEnPargua==0){
+		nWait(m);
+	}
+	barco = enPargua[proxConsumidoPargua];
+	//nPrintf("Barco: %d\n",barco);
+	proxConsumidoPargua = (proxConsumidoPargua + 1) % p;
+	barcosEnPargua--;
+	nNotifyAll(m);
+	nExit(m);
+	return barco;
+}
+
+void producirEnChacao(int x){
+	//nPrintf("Produciendo en chacao de:%d\n",x);
+	nEnter(m);
+	while(barcosEnChacao == p){
+		nWait(m);
+	}
+	enChacao[proxProducidoChacao] = x;
+	proxProducidoChacao = (proxProducidoChacao + 1) % p;
+	barcosEnChacao++;
+	nNotifyAll(m);
+	nExit(m);
+}
+
+void producirEnPargua(int x){
+	//nPrintf("Produciendo en pargua de:%d\n",x);
+	nEnter(m);
+	while(barcosEnPargua == p){
+		nWait(m);
+	}
+	enPargua[proxProducidoPargua] = x;
+	proxProducidoPargua = (proxProducidoPargua + 1) % p;
+	barcosEnPargua++;
+	nNotifyAll(m);
+	nExit(m);
+}
+
 void transbordoAChacao(int v){
-
+	//nPrintf("Transbordo a chacao de :%d\n",v);
+	if(barcosEnChacao == p){
+		transbordoAPargua(-1);
+	}
+	int x = consumirEnPargua();
+	haciaChacao(x,v);
+	producirEnChacao(x);
 }
+
 void transbordoAPargua(int v){
-	
+	//nPrintf("Transbordo a pargua de :%d\n",v);
+	if(barcosEnPargua == p){
+		transbordoAChacao(-1);
+	}
+	int x = consumirEnChacao();
+	haciaPargua(x,v);
+	producirEnPargua(x);
 }
